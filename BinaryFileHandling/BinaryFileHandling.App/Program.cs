d731be3b9cf7a7f55add15d4b4640e62720d775f
rickchensp <71441210+rickchensp@users.noreply.github.com>
2020-11-10 12:59:26 +0000

    created program file

---
+++ BinaryFileHandling/BinaryFileHandling.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BinaryFileHandling.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] content = File.ReadAllBytes("sps.bmp");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
