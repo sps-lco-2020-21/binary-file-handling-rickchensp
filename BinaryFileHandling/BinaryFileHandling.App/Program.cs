db0ecb0c736e4864dd90e36c6fa211d5f9ad0200
rickchensp <71441210+rickchensp@users.noreply.github.com>
2020-11-12 12:24:44 +0000

    updated program.cs

--- BinaryFileHandling/BinaryFileHandling.App/Program.cs
+++ BinaryFileHandling/BinaryFileHandling.App/Program.cs
@@ -12,11 +12,49 @@ namespace BinaryFileHandling.App
     {
         static void Main(string[] args)
         {
-            byte[] content = File.ReadAllBytes("sps.bmp");
+
+            FileSummary();
 
 
             Console.ReadKey();
 
+        }
+
+
+        static void FileSummary()
+        {
+            byte[] content = File.ReadAllBytes("sps.bmp");
+            bool verified = BMPVerify(content);
+            
+            if(!verified)
+            {
+                return;
+            }
+
+            int image_size = BitConverter.ToInt32(content.Skip(2).Take(4).ToArray(), 0);
+            int width = BitConverter.ToInt32(content.Skip(18).Take(4).ToArray(), 0);
+            int height = BitConverter.ToInt32(content.Skip(22).Take(4).ToArray(), 0);
+            int colour_depth = BitConverter.ToInt16(content.Skip(28).Take(2).ToArray(), 0);
+            int horizontal_res = BitConverter.ToInt32(content.Skip(34).Take(4).ToArray(), 0);
+            int vertical_res = BitConverter.ToInt32(content.Skip(38).Take(4).ToArray(), 0);
+
+            Console.Write("the image size is {0} {6}\n the width is {1}\n the height is {2}\n the colour depthis {3}\n " +
+                "the horizontal resolution is {4}\n the vertical resolution is {5}",
+                image_size, width, height, colour_depth, horizontal_res, vertical_res, "bytes");
+        }
+
+        static bool BMPVerify(byte[] content)
+        {
+
+            int res1 = content[0] ^ (byte)'B';
+            int res2 = content[1] ^ (byte)'M';
+
+            if (res1 == 0 && res2 == 0)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
